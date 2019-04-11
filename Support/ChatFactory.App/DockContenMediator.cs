8873805327e9b6bc67db77ff6871a10b07368782
hukuolinpc@outlook.com <hukuolin@outlook.com>
2019-04-11 23:07:56 +0800

    封装的form类与puremvc基础在UI上不能再设计模式下查阅

--- Support/ChatFactory.App/DockContenMediator.cs
+++ Support/ChatFactory.App/DockContenMediator.cs
@@ -7,15 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using PureMVC.Interfaces;
 namespace ChatFactory.App
 {
-    public partial class DockContenMediator : Form
+    public partial class DockContenMediator : Form, IMediator, INotifier
     {
         public DockContenMediator()
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
         }
+        #region Constants
+
+        #endregion
+
+        #region Members
+
+        protected string mediatorName;
+        protected object compontent;
+        #endregion
+
+        #region Constructors
+
+        
+
+
+        #endregion
+
+        #region Accessors
+
+        public virtual string MediatorName
+        {
+            get { return mediatorName; }
+        }
+
+
+        public virtual object ViewComponent
+        {
+            get { return compontent; }
+            set { compontent = value; }
+        }
+
+
+        protected IFacade Facade
+        {
+            get { return m_facade; }
+        }
+        #endregion
+
+        #region Private Members
+
+
+        private IFacade m_facade = FacadeFactory.XPFacadeInstance;
+
+        #endregion
+
+        #region Public Methods
+        #region IMediator Members
+
+        public virtual string[] ListNotificationInterests()
+        {
+            return new string[] { };
+        }
+
+
+        public virtual void HandleNotification(INotification notification)
+        {
+        }
+
+
+        public virtual void OnRegister()
+        {
+        }
+
+
+        public virtual void OnRemove()
+        {
+        }
+        #endregion
+
+        #region INotifier Members
+
+
+
+
+        public virtual void SendNotification(string notificationName, object body, string type)
+        {
+
+            m_facade.SendNotification(notificationName, body, type);
+        }
+        public virtual void InitializeNotifier(string key)
+        {
+
+        }
+        #endregion
+        #endregion
     }
 }
--- Support/ChatFactory.App/FacadeFactory.cs
+++ Support/ChatFactory.App/FacadeFactory.cs
@@ -3,10 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PureMVC.Patterns;
+using PureMVC.Interfaces;
 namespace ChatFactory.App
 {
-    public class FacadeFactory
+
+    public class FacadeFactory : PureMVC.Patterns.Facade.Facade
     {
+        private static FacadeFactory mInstance = null;
+
+        public static FacadeFactory XPFacadeInstance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    mInstance = new FacadeFactory("Facade");
+                }
+
+                return mInstance;
+            }
+        }
+        public FacadeFactory(string key) : base(key)
+        {
+
+        }
+        protected override void InitializeController()
+        {
+            base.InitializeController();
+
+
+        }
+        protected override void InitializeModel()
+        {
+            base.InitializeModel();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
     }
+    public class CommandFactory:PureMVC.Patterns.Command.SimpleCommand
+    {
+
+        public override void Execute(INotification notification)
+        {
+            base.Execute(notification);
+        }
+        public override void SendNotification(string notificationName, object body = null, string type = null)
+        {
+            base.SendNotification(notificationName, body, type);
+        }
+    }
+
+    public class ProxyFactory : IProxy
+    {
+        private string proxyName;
+        private object notifyData;
+        public ProxyFactory(string name)
+        {
+            proxyName = name;
+
+        }
+        public string ProxyName => proxyName;
+
+        public object Data { get => notifyData; set => notifyData = value; }
+
+        public void InitializeNotifier(string key)
+        {
+
+        }
+
+        public void OnRegister()
+        {
+
+        }
+
+        public void OnRemove()
+        {
+
+        }
+
+        public void SendNotification(string notificationName, object body = null, string type = null)
+        {
+
+        }
+    }
+
 }
